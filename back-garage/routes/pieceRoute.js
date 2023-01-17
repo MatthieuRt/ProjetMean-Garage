28f59e5aba2d5aec7aa9f43807f7924d5b6b412c
NainaMatthieu <andrianmattax@gmail.com>
2023-01-17 15:09:40 +0300

    Revert "Fonction pour récuperer piece"
    This reverts commit 5eef7b619d195c7d8e652b542b10edbbba7a03cf.

--- back-garage/routes/pieceRoute.js
+++ back-garage/routes/pieceRoute.js
@@ -3,11 +3,3 @@ const express = require('express');
 const router = express.Router();
 var ObjectId = require('mongoose').Types.ObjectId;
 
-const Piece = require('../models/Piece');
-
-router.get('/:idPiece', (req, res) => {
-    Piece.find({_id:req.params.idPiece},(err, docs) => {
-        if (!err) { res.send(docs); }
-        else { console.log('Erreur : ' + JSON.stringify(err, undefined, 2)); }
-    });
-});
