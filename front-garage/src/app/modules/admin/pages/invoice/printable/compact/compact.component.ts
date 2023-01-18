7b59f0bcdac38078857ae0df19c29ff2067cc7ab
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2023-01-18 09:58:28 +0300

    Récupération des données des factures

--- front-garage/src/app/modules/admin/pages/invoice/printable/compact/compact.component.ts
+++ front-garage/src/app/modules/admin/pages/invoice/printable/compact/compact.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 import { AcademyService } from 'app/modules/admin/apps/academy/academy.service';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
     selector       : 'compact',
     templateUrl    : './compact.component.html',
@@ -14,8 +15,11 @@ export class CompactComponent
      * Constructor
      */
     id : string;
-    constructor(private route: ActivatedRoute)
+    reparation : any;
+    listePieces : any;
+    constructor(private route: ActivatedRoute,private _academyService: AcademyService)
     {
+        
     }
 
     ngOnInit():void{
@@ -23,7 +27,30 @@ export class CompactComponent
             this.route.paramMap.subscribe(params => {
                 this.id = params.get('id');
             });
-            console.log(this.id);
+            
           });
+        
+        this._academyService.getReparationById(this.id).subscribe((res : any)=>{
+            this.reparation = res[0];
+            const listeVoitures = [{ "id": "1", "modele": "Nissan Qashqai" }, { "id": "2", "modele": "Renault Express" }]
+            for(let i = 0; i < listeVoitures.length;i++){
+                if(listeVoitures[i].id==this.reparation.idVoiture){
+                    this.reparation.modele = listeVoitures[i].modele;
+                }
+            }
+            
+            /*for(let i = 0; i < this.reparation.listeReparation.lenghth;i++){
+                const idPiece = this.reparation.listeReparation[i].idPiece;
+                this._academyService.getPieceById(idPiece).subscribe(
+                    piece => {
+                      this.reparation.listeReparation[i].piece = piece;
+                    },
+                    error => {
+                      console.log(error);
+                    }
+                  );
+            }*/
+        })
+        
     }
 }
