abc06af27f6fec89157d26910c30d78140298b84
NainaMatthieu <andrianmattax@gmail.com>
2023-01-23 15:02:48 +0300

    Modèle demande Paiement (back)

--- back-garage/models/DemandePaiement.js
+++ back-garage/models/DemandePaiement.js
@@ -3,7 +3,10 @@ const Piece = require('./Piece');
 const demandePaiementSchema = mongoose.Schema({
     idReparation:String,
     totalDue : String,
-    piece : Piece
+    piece : Piece,
+    date : {
+        type : Date
+    }
 });
 
 module.exports = mongoose.model('DemandePaiement',demandePaiementSchema);
