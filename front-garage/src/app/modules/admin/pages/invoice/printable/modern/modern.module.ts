19d9e186268b6704112049afeb5600c42cef5ba1
NainaMatthieu <andrianmattax@gmail.com>
2023-01-26 15:39:34 +0300

    Revert "Affichage PDF bon de sortie"
    This reverts commit 02d51c514f619629a0db2c92acde34e5401bd1e6.

--- front-garage/src/app/modules/admin/pages/invoice/printable/modern/modern.module.ts
+++ front-garage/src/app/modules/admin/pages/invoice/printable/modern/modern.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { ModernComponent } from 'app/modules/admin/pages/invoice/printable/modern/modern.component';
@@ -13,8 +11,6 @@ import { modernRoutes } from 'app/modules/admin/pages/invoice/printable/modern/m
     imports     : [
         RouterModule.forChild(modernRoutes),
         MatIconModule,
-        MatButtonModule,
-        MatButtonToggleModule,
     ]
 })
 export class ModernModule
