02d51c514f619629a0db2c92acde34e5401bd1e6
NainaMatthieu <andrianmattax@gmail.com>
2023-01-26 14:51:34 +0300

    Affichage PDF bon de sortie

--- front-garage/src/app/modules/admin/pages/invoice/printable/modern/modern.module.ts
+++ front-garage/src/app/modules/admin/pages/invoice/printable/modern/modern.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { ModernComponent } from 'app/modules/admin/pages/invoice/printable/modern/modern.component';
@@ -11,6 +13,8 @@ import { modernRoutes } from 'app/modules/admin/pages/invoice/printable/modern/m
     imports     : [
         RouterModule.forChild(modernRoutes),
         MatIconModule,
+        MatButtonModule,
+        MatButtonToggleModule,
     ]
 })
 export class ModernModule
